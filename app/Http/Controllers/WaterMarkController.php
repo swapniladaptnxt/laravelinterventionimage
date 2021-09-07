dd810ea7b6da5ce081606e8751167e9f168c0032
swapnil yadav <swapnil@adaptnxt.com>
2021-09-07 11:58:56 +0530

    update resize

--- app/Http/Controllers/WaterMarkController.php
+++ app/Http/Controllers/WaterMarkController.php
@@ -1,22 +1,30 @@
 <?php
 namespace App\Http\Controllers;
 
-use Image;
 use App\Models\ImageModel;
 use Illuminate\Http\Request;
+use Image;
 
 class WaterMarkController extends Controller
 {
     public function imageWatermark(Request $request)
-{
-    
     {
-        $this->validate($request, [
-            'filename' => 'image|required|mimes:jpeg,png,jpg,gif,svg'
-         ]);
-    }
-        $img  = Image::make($request->file('filename'))->blur(40)->resize($request->height, $request->width);
-        $img1 = Image::make($request->file('filename'))->resize(450, 400);
+
+        {
+            $this->validate($request, [
+                'filename' => 'image|required|mimes:jpeg,png,jpg,gif,svg',
+            ]);
+        }
+
+        $img                            = Image::make($request->file('filename'))->blur(40)->resize($request->height, $request->width);
+        list($width_orig, $height_orig) = getimagesize($request->file('filename'));
+        $ratio_orig                     = $width_orig / $height_orig;
+        if ( $request->width / $request->height > $ratio_orig) {
+            $width = $request->height * $ratio_orig;
+        } else {
+            $height =  $request->width / $ratio_orig;
+        }
+        $img1 = Image::make($request->file('filename'))->resize($request->width, $height);
         // $img1->fit(800, 600, function ($constraint) {
         //     $constraint->upsize();
         // });
@@ -27,36 +35,35 @@ class WaterMarkController extends Controller
         $img->encode('png');
         $type      = 'png';
         $new_image = 'data:image/' . $type . ';base64,' . base64_encode($img);
-        return view('show_watermark',compact('new_image'));
+        return view('show_watermark', compact('new_image'));
     }
 
-    public function create(){
+    public function create()
+    {
         $image = ImageModel::latest()->first();
         return view('createimage', compact('image'));
     }
 
     public function store(Request $request)
     {
-      
+
         {
             $this->validate($request, [
-                'filename' => 'image|required|mimes:jpeg,png,jpg,gif,svg'
-             ]);
+                'filename' => 'image|required|mimes:jpeg,png,jpg,gif,svg',
+            ]);
         }
-        $originalImage= $request->file('filename');
+        $originalImage  = $request->file('filename');
         $thumbnailImage = Image::make($originalImage);
-        $thumbnailPath = public_path().'/thumbnail/';
-        $originalPath = public_path().'/images/';
-        $thumbnailImage->save($originalPath.time().$originalImage->getClientOriginalName());
-        $thumbnailImage->resize(200,200)->blur(30);
-      
+        $thumbnailPath  = public_path() . '/thumbnail/';
+        $originalPath   = public_path() . '/images/';
+        $thumbnailImage->save($originalPath . time() . $originalImage->getClientOriginalName());
+        $thumbnailImage->resize(200, 200)->blur(30);
 
-        $thumbnailImage->save($thumbnailPath.time().$originalImage->getClientOriginalName()); 
+        $thumbnailImage->save($thumbnailPath . time() . $originalImage->getClientOriginalName());
 
-        $imagemodel= new ImageModel();
-        $imagemodel->filename=time().$originalImage->getClientOriginalName();
+        $imagemodel           = new ImageModel();
+        $imagemodel->filename = time() . $originalImage->getClientOriginalName();
         $imagemodel->save();
-
 
         return back()->with('success', 'Your images has been successfully Upload');
 
